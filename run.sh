b6a015e693360b7f2a2755e0ad9862fa39aa3657
theofff <94871002+theofff@users.noreply.github.com>
2022-03-25 15:27:15 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-winpty python3 /mysite/manage.py createsuperuser <<EOF
-$ADMIN
-admin@example.com
-$ADMIN_PASSWORD
-$ADMIN_PASSWORD
-EOF
+#winpty python3 /mysite/manage.py createsuperuser <<EOF
+#$ADMIN
+#admin@example.com
+#$ADMIN_PASSWORD
+#$ADMIN_PASSWORD
+#EOF
 python3 /mysite/manage.py migrate
 python3 /mysite/manage.py runserver 0.0.0.0:8083
